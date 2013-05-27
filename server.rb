8d498e9a690896768d1b8aab4aba9cda94a98aab
Dave Haeffner <dave@arrgyle.com>
2013-05-27 16:11:49 -0400

    Made it so the form would submit and return a message stating that the file was ignored.

--- server.rb
+++ server.rb
@@ -3,3 +3,10 @@ require 'sinatra'
 get '/upload' do
   erb :upload
 end
+
+post '/upload' do
+#  File.open('uploads/' + params['myfile'][:filename], 'w') do |f|
+#    f.write(params['myfile'][:filemame].read)
+#  end
+  return "The file was ignored."
+end
