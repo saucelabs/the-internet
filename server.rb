83ae9176540900d2a19aac464bddfd0a42173aee
dave@arrgyle.com <dave@arrgyle.com>
2013-06-02 13:57:50 -0400

    Added a simple mime type setter based on a couple of different filetypes (jpg and pdf) and have a catch-all for octet-stream if the file type is something unrecognized

--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@ get '/upload' do
   erb :upload
 end
 
+p
 post '/upload' do
   file = params['myfile']
   File.open('public/uploads/' + file[:filename], 'w') do |f|
@@ -18,7 +19,21 @@ get '/download' do
 end
 
 get '/download/:filename' do |filename|
+  mime_type = get_mime_type_for(filename)
+
   send_file "public/uploads/#{filename}",
     :filename => filename,
-    :type => 'Application/octet-stream'
+    :type => mime_type
+end
+
+def get_mime_type_for(filename)
+  file_type = filename.split('.').last
+  case file_type
+    when 'jpg'
+      'image/jpeg'
+    when 'pdf'
+      'application/pdf'
+    else
+      'application/octet-stream'
+  end
 end
