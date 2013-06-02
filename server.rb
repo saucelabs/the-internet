f796895378e6b1e3f205ee7166d3c5e959289cb5
dave@arrgyle.com <dave@arrgyle.com>
2013-06-02 09:44:45 -0400

    Added downloading a file. Creates list from uploaded file dir

--- server.rb
+++ server.rb
@@ -9,5 +9,16 @@ post '/upload' do
   File.open('public/uploads/' + file[:filename], 'w') do |f|
     f.write(file[:tempfile].read)
   end
-  erb :uploaded, locals: {file: file}
+  erb :uploaded, locals: { file: file }
+end
+
+get '/download' do
+  @file_list = Dir.glob("public/uploads/*.*").map { |f| f.split('/').last }
+  erb :download
+end
+
+get '/download/:filename' do |filename|
+  send_file "public/uploads/#{filename}",
+    :filename => filename,
+    :type => 'Application/octet-stream'
 end
