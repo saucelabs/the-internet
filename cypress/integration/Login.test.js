0ebbd6b31aa9ca543ee8a58ac5e52c7e9604e4d2
Virginia Ladd <virginialadd2@gmail.com>
2020-08-13 09:55:22 -0600

    Write first test to check input box functionality

--- cypress/integration/Login.test.js
+++ cypress/integration/Login.test.js
@@ -3,11 +3,20 @@ describe('Login', () => {
     cy.visit('https://the-internet.herokuapp.com/login')
   });
   context('High Priority', () => {
-
+    it('Should be able to fill in the inputs', () => {
+      // Testing a standard username input
+      // Finding the input by name value
+      cy.get('[name=username]').type('Username');
+      cy.get('[name=username]').should('have.value', 'Username');
+      // Testing a standard password input
+      // Finding the input by index value
+      cy.get('input').eq(1).type('Password');
+      cy.get('input').eq(1).should('have.value', 'Password');
+    });
 
   });
 
   context('Low Priority', () => {
-    
+
   });
 });
