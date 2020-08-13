bbd3eb859e5e64e17f4d6836caccbe3647858427
Virginia Ladd <virginialadd2@gmail.com>
2020-08-13 09:54:46 -0600

    Seyup testing suite with describe block and context blocks

---
+++ cypress/integration/Login.test.js
@@ -0,0 +1,13 @@
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login')
+  });
+  context('High Priority', () => {
+
+
+  });
+
+  context('Low Priority', () => {
+    
+  });
+});
