29da39dcb8434b11274b99cb4cef3d734345ada7
Carlos Gutierrez <carlos@spartan-testsolutions.co.uk>
2022-07-05 15:18:34 +0100

    Fixed failing tests

--- spec/pages/dynamic_loading.rb
+++ spec/pages/dynamic_loading.rb
@@ -2,15 +2,15 @@ require_relative 'page'
 
 class DynamicLoading < Page
 
-  START_BUTTON  = { css: '#start button' }
-  FINISH_TEXT   = { id: 'finish' }
+  START_BUTTON = { css: '#start button' }
+  FINISH_TEXT = { id: 'finish' }
 
   def example(example_number)
-    visit "http://the-internet.herokuapp.com/dynamic_loading/#{example_number}"
+    visit ("/dynamic_loading/#{example_number}")
   end
 
   def start
-    click START_BUTTON
+    click(START_BUTTON)
   end
 
   def finish_text_present?
@@ -18,4 +18,3 @@ class DynamicLoading < Page
   end
 
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,43 +1,15 @@
+require 'rspec'
 require 'selenium-webdriver'
-require 'sauce_whisk'
+require 'webdrivers/chromedriver'
 
 RSpec.configure do |config|
-
   config.before(:each) do |example|
-    ENV['host']             ||= 'saucelabs'
-    ENV['base_url']         ||= 'http://the-internet.herokuapp.com'
-    ENV['operating_system'] ||= 'Windows 10'
-    ENV['browser']          ||= 'firefox'
-    ENV['browser_version']  ||= '50.0'
-
-    case ENV['host']
-    when 'localhost'
-      @driver = Selenium::WebDriver.for :chrome
-    when 'saucelabs'
-      caps = Selenium::WebDriver::Remote::Capabilities.send(ENV['browser'])
-      caps[:platform]  = ENV['operating_system']
-      caps[:version]   = ENV['browser_version']
-      caps[:name]      = example.metadata[:full_description]
-      @driver = Selenium::WebDriver.for(
-        :remote,
-        url: "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub",
-        desired_capabilities: caps)
-    end
+    ENV['base_url'] ||= 'http://localhost:9292'
+    options = Selenium::WebDriver::Chrome::Options.new(options: { w3c: true })
+    @driver = Selenium::WebDriver.for(:chrome, options: options)
   end
 
   config.after(:each) do |example|
-    begin
-      if ENV['host'] == 'saucelabs'
-        if example.exception.nil?
-          SauceWhisk::Jobs.pass_job @driver.session_id
-        else
-          SauceWhisk::Jobs.fail_job @driver.session_id
-          raise "Watch a video of the test at https://saucelabs.com/tests/#{@driver.session_id}"
-        end
-      end
-    ensure
-      @driver.quit
-    end
+    @driver.close
   end
-
 end
